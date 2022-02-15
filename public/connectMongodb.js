8c6f0f14618f78ac8138e63bc25650168f895e8d
steerc2217 <97324100+steerc2217@users.noreply.github.com>
2022-02-15 03:44:00 +0300

    mongo

--- public/connectMongodb.js
+++ public/connectMongodb.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 const mongoDB = 'mongodb://127.0.0.1/xpoesy_database'
 
-mongoose.connect(mongoDB, {useNewUrlParser : true, useUnifiedTopology : true})
+mongoose.connect(mongoDB, {useNewUrlParser : true, useUnifiedTopology : true}).then(() => {
+    console.log('Connected mongodb')
+})
 const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-module.exports = db;
+// module.exports = db;
